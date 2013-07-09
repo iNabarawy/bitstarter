61fca45f2708dd17d08db651bf02bb21dfd7db54
Ahmed T.Nabarawy <iNabarawy@gmail.com>
2013-07-09 13:19:29 +0000

    index

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFile('index.html',null,null));
+  response.send(fs.readFile('index.html',null,null).toString('utf8',null,null));
 });
 
 var port = process.env.PORT || 5000;
